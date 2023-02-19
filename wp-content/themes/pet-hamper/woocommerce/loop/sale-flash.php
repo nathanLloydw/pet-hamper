4ce5d837087407dd9abf35315a2bce46f9af1841
nlloydw <nlloydw61@gmail.co.uk>
2023-02-19 19:04:46 +0000

    added a sale percent off banner

---
+++ wp-content/themes/pet-hamper/woocommerce/loop/sale-flash.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Product loop sale flash
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/loop/sale-flash.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see         https://docs.woocommerce.com/document/template-structure/
+ * @package     WooCommerce\Templates
+ * @version     1.6.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+global $post, $product;
+
+?>
+<?php if ( $product->is_on_sale() ) : ?>
+
+    <?php
+    $sale_price = $product->get_sale_price();
+    $regular_price = $product->get_regular_price();
+
+    if($sale_price != "" && $regular_price !="")
+    {
+        $sale_percent = round((($regular_price - $sale_price) / $regular_price) * 100,2);
+        echo apply_filters( 'woocommerce_sale_flash', '<span class="onsale test2" data-sale="'.$sale_price.'" data-reg="'.$regular_price.'">' . esc_html__( 'Sale! '.$sale_percent.'% off!', 'woocommerce' ) . '</span>', $post, $product );
+    }
+    else
+    {
+        echo apply_filters( 'woocommerce_sale_flash', '<span class="onsale test2" data-sale="'.$sale_price.'" data-reg="'.$regular_price.'">' . esc_html__( 'Sale!', 'woocommerce' ) . '</span>', $post, $product );
+    }
+
+
+
+endif;
+
+/* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
