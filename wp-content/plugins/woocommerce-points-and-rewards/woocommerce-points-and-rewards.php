7e7463b53003002690ce64d67c15f0b515cb951b
nlloydw <nlloydw61@gmail.co.uk>
2023-06-20 19:41:41 +0100

    updated woo points & rewards

--- wp-content/plugins/woocommerce-points-and-rewards/woocommerce-points-and-rewards.php
+++ wp-content/plugins/woocommerce-points-and-rewards/woocommerce-points-and-rewards.php
@@ -5,11 +5,11 @@
  * Description: Reward customers for purchases and other actions with points which can be redeemed for discounts
  * Author: WooCommerce
  * Author URI: https://woocommerce.com
- * Version: 1.7.33
+ * Version: 1.7.34
  * Text Domain: woocommerce-points-and-rewards
  * Domain Path: /languages/
  * Tested up to: 6.2
- * WC tested up to: 7.7
+ * WC tested up to: 7.8
  * WC requires at least: 4.5
  *
  * Copyright: © 2023 WooCommerce
@@ -155,7 +155,7 @@ register_activation_hook( __FILE__, 'wc_points_rewards_activate' );
  */
 
 if ( ! class_exists( 'WC_Points_Rewards' ) ) :
-	define( 'WC_POINTS_REWARDS_VERSION', '1.7.33' ); // WRCS: DEFINED_VERSION.
+	define( 'WC_POINTS_REWARDS_VERSION', '1.7.34' ); // WRCS: DEFINED_VERSION.
 	define( 'WC_POINTS_REWARDS_ENDPOINT', 'points-and-rewards' );
 
 	class WC_Points_Rewards {
